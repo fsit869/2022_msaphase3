3301bfe1e095b33882a49aa6a425e481a0ed5fdb
fsit869 <fsit869@aucklanduni.ac.nz>
2022-08-17 16:56:50 +1200

    Added Note repo

---
+++ Backend/src/main/java/com/frank/msaphase3/repository/NoteRepository.java
@@ -0,0 +1,7 @@
+package com.frank.msaphase3.repository;
+
+import com.frank.msaphase3.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
