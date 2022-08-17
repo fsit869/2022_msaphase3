d4ecaa8148c3551762b6e17b34f6bb67c91029ee
fsit869 <fsit869@aucklanduni.ac.nz>
2022-08-17 16:43:48 +1200

    Added template backend spring

---
+++ Backend/src/main/java/com/frank/msaphase3/Msaphase3Application.java
@@ -0,0 +1,12 @@
+package com.frank.msaphase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Msaphase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msaphase3Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/frank/msaphase3/Msaphase3ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.frank.msaphase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class Msaphase3ApplicationTests {
+
+	@Test void contextLoads() {
+	}
+
+}
